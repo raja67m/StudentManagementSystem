b050a1698740c691e2960290f262f7d2f56f79f3
Raja <rajalingam210901@gmail.com>
2025-03-26 20:09:39 +0530

    added update button function and css style changes

--- curd.js
+++ curd.js
@@ -1,18 +1,19 @@
-let studentName=document.getElementById('studentName');
-let studentDept=document.getElementById('studentDept');
-let studentAge=document.getElementById('studentAge');
-let studentEmail=document.getElementById('studentEmail');
-let saveBtn=document.getElementById('save-btn');
-let cancelBtn=document.getElementById('cancel');
-let studentData=document.getElementById('studentData');
 
+let saveBtn=document.getElementById('save-btn').value;
+let cancelBtn=document.getElementById('cancel').value;
+let studentData=document.getElementById('studentData');
+var studentName=document.getElementById('studentName');
+var studentDept=document.getElementById('studentDept');
+var studentAge=document.getElementById('studentAge');
+var studentEmail=document.getElementById('studentEmail');
 
 let data=[
-   {id:1,stdName:"Rajalingam",stdDept:"IT",stdAge:23,stdEmail:"raj12@gmail.com"},
-   {id:2,stdName:"Raja",stdDept:"IT",stdAge:23,stdEmail:"rajeh12@gmail.com"},
+   {id:1,studentName:"Rajalingam",studentDept:"IT",studentAge:23,studentEmail:"raj12@gmail.com"},
+   {id:2,studentName:"Raja",studentDept:"IT",studentAge:23,studentEmail:"rajeh12@gmail.com"},
 
 ];
 
+// display data to table
 function readAll(){
    localStorage.setItem('object',JSON.stringify(data));
    let object=localStorage.getItem('object');
@@ -25,13 +26,46 @@ function readAll(){
       <td>${record.studentName}</td>
       <td>${record.studentDept}</td>
       <td>${record.studentAge}</td>
-      <td>${x.stdEmail}</td>
+      <td>${record.studentEmail}</td>
     <td>
-   <button class="edit-btn"><i class='bx bxs-edit'></i>Edit</button>
-   <button class="delete-btn"><i class='bx bxs-trash'></i>Delete</button>
+   <button type="button" class="edit-btn" onclick={edit(${record.id})}><i class='bx bxs-edit'></i>Edit</button>
+   <button type="button" class="delete-btn"><i class='bx bxs-trash'></i>Delete</button>
                </td>
       </tr>
       `
-   ))
+   ));
 
+   studentData.innerHTML=elements;
+
+
+}
+// create a  new record
+
+function createRecord(){
+   var studentName=document.querySelector('.studentName').value;
+   var studentDept=document.querySelector('.studentDept').value;
+   var studentAge=document.querySelector('.studentAge').value;
+   var studentEmail=document.querySelector('.studentEmail').value;
+   var addData={id:3,studentName:studentName,studentAge:studentAge,studentDept:studentDept,studentEmail:studentEmail};
+
+data.push(addData);
+console.log(addData)
+readAll();
+resetForm();
+}
+
+// edit function
+
+function edit(id){
+document.querySelector('.cancel').style.display="block";
+document.querySelector('.save-btn').style.display="none";
+
+}
+
+// reset form
+function resetForm(){
+   studentName.value="",
+   studentAge.value="",
+   studentDept.value="",
+   studentEmail.value=""
 }
